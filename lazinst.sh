4221be88bab47df40b3643ef8f7e2cacac399156
GreyDreamYT <78270259+GreyDreamYT@users.noreply.github.com>
2021-10-16 13:44:08 +0530

    Add files via upload

---
+++ lazinst.sh
@@ -0,0 +1,66 @@
+#packages
+#created By GreyDreamYT
+#Single Click Package Downloader
+#Internet Consumed :- Not Defined
+#Date created :-15 October 2021
+#Owner :- GD
+pkg install curl
+pkg install perl
+pkg install php
+pkg install golang
+pkg install clang
+pkg install python
+pkg install openjdk-17
+pkg install ruby
+pkg install rust
+pkg install nodejs
+pkg install quikjs
+pkg install duktape
+pkg install ecj
+pkg install bash
+pkg install sl
+pkg install cmatrix
+pkg install nano
+pkg install git
+pkg install libcaca
+pkg install figlet
+pkg install toilet
+pkg install cowsay
+pkg install w3m
+pkg install telnet
+pkg install fish
+pkg install fortune
+pkg install tmate
+pkg install wget
+pkg install metasploit
+pkg install terminage
+pkg install tmate
+#folders
+mkdir bomb
+mkdir wifi
+mkdir camera
+mkdir mobile
+mkdir ip
+mkdir attack
+mkdir instagram
+mkdir whatsapp
+mkdir facebook
+mkdir universal
+#tools
+cd bomb
+git clone https://github.com/TheSpeedX/TBomb
+git clone https://github.com/anubhavanonymous/XLR8_BOMBER
+git clone https://github.com/AvinashReddy3108/YetAnotherSMSBomber.git
+git clone https://github.com/adi1090x/termux-style
+git clone https://github.com/s0md3v/ReconDogt
+pip install lolcat requests
+cd ..
+cd ip
+git clone https://github.com/noob-hackers/ipdrone
+cd ..
+cd universal
+git clone https://github.com/khansaad1275/maskphish
+git clonehttps://github.com/s0md3v/ReconDog
+mkdir instagram
+git clone https://github.com/zxllkada/Get-instagram-users-info---Any-instagram-account
+git clone https://github.com/th3unkn0n/osi.ig.git
